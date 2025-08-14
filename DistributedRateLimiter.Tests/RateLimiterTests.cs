4b6744e61860fd0baadc100a1ece5c416bb2ab94
Aditya Palpattuwar <adityapalpattuwar7@gmail.com>
2025-08-14 22:50:28 +0530

    Add unit test

--- DistributedRateLimiter.Tests/RateLimiterTests.cs
+++ DistributedRateLimiter.Tests/RateLimiterTests.cs
@@ -1,4 +1,6 @@
-﻿using Moq;
+using DistributedRateLimiter.Configuration;
+using Microsoft.Extensions.Options;
+using Moq;
 using StackExchange.Redis;
 
 namespace DistributedRateLimiter.Tests;
@@ -16,7 +18,7 @@ public class RateLimiterTests
         var limiter = CreateLimiter(databaseMock.Object);
 
         // Act
-        var (allowed, remaining, retryAfterSec) = await limiter.AllowRequestAsync("user:1", 5, 1);
+        var (allowed, remaining, retryAfterSec) = await limiter.AllowRequestAsync("user:1");
 
         // Assert
         Assert.True(allowed);
@@ -40,7 +42,7 @@ public class RateLimiterTests
         var limiter = CreateLimiter(dbMock.Object);
 
         // Act
-        var (allowed, remaining, retryAfterSec) = await limiter.AllowRequestAsync("user:1", 5, 1);
+        var (allowed, remaining, retryAfterSec) = await limiter.AllowRequestAsync("user:1");
 
         // Assert
         Assert.True(allowed);
@@ -48,13 +50,37 @@ public class RateLimiterTests
         Assert.Equal(0, retryAfterSec);
     }
     
+    [Fact]
+    public async Task RequestWhenEmpty_ShouldDeny()
+    {
+        // Arrange
+        var dbMock = new Mock<IDatabase>();
+        var now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        dbMock.Setup(db => db.HashGetAsync(It.IsAny<RedisKey>(), It.IsAny<RedisValue[]>(), CommandFlags.None))
+            .ReturnsAsync([0, now]);
+
+        var limiter = CreateLimiter(dbMock.Object);
+
+        // Act
+        var (allowed, remaining, retryAfterSec) = await limiter.AllowRequestAsync("user:1");
+
+        // Assert
+        Assert.False(allowed);
+        Assert.Equal(0, remaining);
+        Assert.True(retryAfterSec >= 1);
+    }
+    
     private static RateLimiter CreateLimiter(IDatabase db)
     {
         var muxerMock = new Mock<IConnectionMultiplexer>();
+        
+        var rateLimiterOptions = new RateLimiterOptions { BurstCapacity = 5, RefillPerSecond = 1};
+        var options = Options.Create(rateLimiterOptions);
+
         muxerMock
             .Setup(m => m.GetDatabase(It.IsAny<int>(), It.IsAny<object>()))
             .Returns(db);
 
-        return new RateLimiter(muxerMock.Object);
+        return new RateLimiter(muxerMock.Object, options);
     }
 }
